942061e549cf6fc525cff63665e43ef0057aee4e
Robert Howard <52471320+rhoward314@users.noreply.github.com>
2019-07-29 17:19:23 -0400

    Added svg path

--- assignments/final_project/clock.js
+++ assignments/final_project/clock.js
@@ -1,7 +1,4 @@
 let clock = document.getElementById("clock");
-let hours = document.getElementById("hours");
-let minutes = document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
 let body = document.querySelector("body");
 
 function findTime() {
@@ -22,9 +19,9 @@ function findTime() {
   }
 
   clock.innerHTML = `${hh}:${mm}:${ss}`;
-  clock.style.left = `${1.33 * sms}vw`;
-  clock.style.top = `${1.46 * sms}vh`;
-  clock.style.fontSize = `${sms + 10}px`;
+  clock.style.left = `${sms}vw`;
+  clock.style.top = `${sms}vh`;
+  clock.style.fontSize = `${-24 * Math.cos(Math.PI * (m + (s / 60)) / 30) + 48}px`
   clock.style.color = `rgb(${4*sms},${4*sms},${4*sms})`;
   body.style.background = `rgb(${255-(4*sms)},${255-(4*sms)},${255-(4*sms)})`;
 }
